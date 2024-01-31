658eac8f3b3dd36a480b79178db1770f6e364080
DinhQuocDat123 <118040503+DinhQuocDat123@users.noreply.github.com>
2024-01-31 22:52:10 +0700

    logo

--- DuAnBanAo/src/main/java/com/example/demo/controllers/HomeController.java
+++ DuAnBanAo/src/main/java/com/example/demo/controllers/HomeController.java
@@ -7,9 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class HomeController {
     @Autowired
     private ChiTietSanPhamRepository chiTietSanPhamRepository;
